56a4dd713d7cd3eb7ffa94134c89fca713662702
david huveau <davidhuveau@macbook-pro-davidnew.home>
2023-12-24 10:29:32 +0100

    type testing

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    string str1("Hello"), str2("world");
+    char char1 = '!';
+    cout << str1 << ' ' << str2 << char1 << endl;
+
+    cout << "Size of int type in bytes: " << sizeof(double) << endl;
+    cout << "Lower limit of int : " << numeric_limits<double>::min() << endl;
+    cout << "Upper limit of int : " << numeric_limits<double>::max() << endl;
+
     return 0;
 }
